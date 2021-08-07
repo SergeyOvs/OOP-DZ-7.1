b5bef287a82f8fa3e3312ce4bfc4f83c10ec91fa
Sergey Ovsyannikov <82229027+SergeyOvs@users.noreply.github.com>
2021-08-07 14:15:55 +0300

    Добавьте файлы проекта.

---
+++ OOP DZ 7.1/OOP DZ 7.1.cpp
@@ -0,0 +1,39 @@
+﻿ #include<iostream>
+using namespace std;
+
+class Date
+{
+private:
+	int day, month, year;
+public:
+	Date(int date_day, int date_month, int date_year)
+	{
+		void setDate(int date_day, int date_month, int date_year);
+	{ 
+		day = date_day;
+		month = date_month;
+		year = date_year;
+	}
+	void getDate();
+	{
+		cout << " Date : " << day << "." << month << "." << year << "." << endl;
+	}
+	
+	
+	}
+
+	~Date();
+
+};
+
+
+int main(int argc,char*argv[])
+{
+	int day = 20;
+	int month = 07;
+	int year = 2021;
+	Date todayDate();
+	Date getDate();
+
+	return 0;
+}
